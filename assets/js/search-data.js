57276ae12f8883f61312253dabdea74678550147
intricate-potato <50263968+intricate-potato@users.noreply.github.com>
2025-09-24 02:35:16 +0000

    Deploying to gh-pages from @ intricate-potato/intricate-potato.github.io@afb6af8eba56a30e4278ee28b81325405f52e9a9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,8 +44,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-teaching",
+          title: "teaching",
           description: "",
           section: "Navigation",
           handler: () => {
